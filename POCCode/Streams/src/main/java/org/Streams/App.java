e3ed237e477faea81f22ff14c7b70924e9757ca7
kdeepak <kdeepak@hotmail.com>
2017-07-23 12:00:13 +0530

    added new module for Java Stream

---
+++ POCCode/Streams/src/main/java/org/Streams/App.java
@@ -0,0 +1,13 @@
+package org.Streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
