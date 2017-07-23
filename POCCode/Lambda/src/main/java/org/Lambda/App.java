8fbce1c03e4b2b446e50de5ab6dde7b1ed923bd9
kdeepak <kdeepak@hotmail.com>
2017-07-23 08:23:55 +0530

    create empty project for POCs

---
+++ POCCode/Lambda/src/main/java/org/Lambda/App.java
@@ -0,0 +1,13 @@
+package org.Lambda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
