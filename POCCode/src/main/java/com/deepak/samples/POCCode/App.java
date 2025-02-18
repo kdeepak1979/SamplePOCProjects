88bb8e108cfb11c5ba788032b43d3f246638b23d
Deepak Kumar <kdeepak@hotmail.com>
2025-02-18 22:01:47 +0530

    Test Commit

--- POCCode/src/main/java/com/deepak/samples/POCCode/App.java
+++ POCCode/src/main/java/com/deepak/samples/POCCode/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Core Java" );
-        System.out.println( "Core Java" );
+        System.out.println( "Core Java2" );
     }
 }
